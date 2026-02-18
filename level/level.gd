52546188243687f8fe62b75207983b1d2567ffae
Felix Wrba <felix.wrba@web.de>
2026-02-18 17:04:39 +0100

    Added chunk rendering

--- level/level.gd
+++ level/level.gd
@@ -11,10 +11,10 @@ var chunks: Dictionary = {}
 
 enum Voxel { AIR, GRASS, DIRT, STONE }
 
-var surface_array: Array = []
-var vertices = PackedVector3Array()
-var normals = PackedVector3Array()
-var colors = PackedColorArray()
+var c_surface_array: Array = []
+var c_vertices = PackedVector3Array()
+var c_normals = PackedVector3Array()
+var c_colors = PackedColorArray()
 
 var cube_vertices: Array[Vector3] = [
 	Vector3(-0.5, -0.5, 0.5),
@@ -57,7 +57,7 @@ var face_colors: Dictionary[Face, Color] = {
 }
 
 func _ready() -> void:
-	surface_array.resize(Mesh.ARRAY_MAX)
+	c_surface_array.resize(Mesh.ARRAY_MAX)
 	generate_mesh()
 	
 func generate_mesh() -> void:
@@ -73,15 +73,15 @@ func add_face(face: Face, position: Vector3) -> void:
 	var indices := face_indices[face]
 	for triangle in indices:
 		for index in triangle:
-			vertices.append(cube_vertices[index] + position)
-			normals.append(face_normals[face])
-			colors.append(face_colors[face])
+			c_vertices.append(cube_vertices[index] + position)
+			c_normals.append(face_normals[face])
+			c_colors.append(face_colors[face])
 
 func commit_mesh() -> void:
-	surface_array[Mesh.ARRAY_VERTEX] = vertices
-	surface_array[Mesh.ARRAY_NORMAL] = normals
-	surface_array[Mesh.ARRAY_COLOR] = colors
-	mesh.mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
+	c_surface_array[Mesh.ARRAY_VERTEX] = c_vertices
+	c_surface_array[Mesh.ARRAY_NORMAL] = c_normals
+	c_surface_array[Mesh.ARRAY_COLOR] = c_colors
+	mesh.mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, c_surface_array)
 	mesh.mesh.surface_set_material(0, cube_material)
 
 func _process(_delta: float) -> void:
@@ -101,7 +101,8 @@ func remove_chunks(chunk_pos: Vector3):
 	
 	for chunk_x in chunks:
 		for chunk_z in chunks[chunk_x]:
-			if chunk_x != clamp(chunk_x, start_x, end_x) or chunk_z != clamp(chunk_z, start_z, end_z):
+			if chunk_x < start_x or chunk_x > end_x or chunk_z < start_z or chunk_z > end_z:
+				chunks[chunk_x][chunk_z].mesh.queue_free()
 				chunks[chunk_x].erase(chunk_z)
 		if chunks[chunk_x].size() == 0:
 			chunks.erase(chunk_x)
@@ -112,13 +113,16 @@ func spawn_chunks(chunk_pos: Vector3) -> void:
 			chunks[x] = {}
 		for z in range(chunk_pos.z - RENDER_DISTANCE, chunk_pos.z + RENDER_DISTANCE):
 			if not chunks[x].has(z):
-				var new_chunk := create_chunk(chunk_pos)
-				create_mesh(new_chunk)
-				chunks[x][z] = new_chunk
+				var new_chunk_data := create_chunk_data(chunk_pos)
+				var new_chunk_mesh := create_chunk_mesh(new_chunk_data, chunk_pos)
+				chunks[x][z] = {
+					'mesh': new_chunk_mesh, 
+					'data': new_chunk_data
+				}
 
-func create_chunk(_chunk_pos: Vector3) -> Dictionary[Vector3, Voxel]:
+func create_chunk_data(_chunk_pos: Vector3) -> Dictionary[Vector3, Voxel]:
 	var chunk: Dictionary[Vector3, Voxel] = {}
-	const GROUND_LEVEL = 0
+	const GROUND_LEVEL = 10
 	const DIRT_HEIGHT = 4
 	
 	for x in range(CHUNK_SIZE.x):
@@ -132,8 +136,70 @@ func create_chunk(_chunk_pos: Vector3) -> Dictionary[Vector3, Voxel]:
 					chunk[Vector3(x, y, z)] = Voxel.DIRT
 				else:
 					chunk[Vector3(x, y, z)] = Voxel.STONE
-					
 	return chunk
 
-func create_mesh(chunk: Dictionary[Vector3, Voxel]) -> void:
-	pass
+func create_chunk_mesh(chunk_data: Dictionary[Vector3, Voxel], chunk_pos: Vector3) -> MeshInstance3D:
+	var vertices := PackedVector3Array()
+	var normals := PackedVector3Array()
+	var colors := PackedColorArray()
+	
+	for pos in chunk_data:
+		var voxel: Voxel = chunk_data[pos]
+		if voxel == Voxel.AIR:
+			continue
+		
+		var world_pos = Vector3(
+			chunk_pos.x * CHUNK_SIZE.x + pos.x,
+			pos.y,
+			chunk_pos.z * CHUNK_SIZE.z + pos.z,
+		)
+		
+		if is_air(chunk_data, pos + Vector3.UP):
+			add_face_to_arrays(Face.TOP, world_pos, vertices, normals, colors)
+		if is_air(chunk_data, pos + Vector3.DOWN):
+			add_face_to_arrays(Face.BOTTOM, world_pos, vertices, normals, colors)
+		if is_air(chunk_data, pos + Vector3.LEFT):
+			add_face_to_arrays(Face.LEFT, world_pos, vertices, normals, colors)
+		if is_air(chunk_data, pos + Vector3.RIGHT):
+			add_face_to_arrays(Face.RIGHT, world_pos, vertices, normals, colors)
+		if is_air(chunk_data, pos + Vector3.FORWARD):
+			add_face_to_arrays(Face.FRONT, world_pos, vertices, normals, colors)
+		if is_air(chunk_data, pos + Vector3.BACK):
+			add_face_to_arrays(Face.BACK, world_pos, vertices, normals, colors)
+	
+	var surface_array := []
+	surface_array.resize(Mesh.ARRAY_MAX)
+	
+	surface_array[Mesh.ARRAY_VERTEX] = vertices
+	surface_array[Mesh.ARRAY_NORMAL] = normals
+	surface_array[Mesh.ARRAY_COLOR] = colors
+	
+	var array_mesh = ArrayMesh.new()
+	array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
+	array_mesh.surface_set_material(0, cube_material)
+	
+	var mesh_instance := MeshInstance3D.new()
+	mesh_instance.mesh = array_mesh
+	add_child(mesh_instance)
+	
+	return mesh_instance
+
+func is_air(chunk_data: Dictionary[Vector3, Voxel], position: Vector3) -> bool:
+	if not chunk_data.has(position):
+		return true
+	
+	return chunk_data[position] == Voxel.AIR
+
+func add_face_to_arrays(
+	face: Face, 
+	face_position: Vector3,
+	vertices: PackedVector3Array, 
+	normals: PackedVector3Array, 
+	colors: PackedColorArray,
+) -> void:
+	var indices := face_indices[face]
+	for triangle in indices:
+		for index in triangle:
+			vertices.append(cube_vertices[index] + face_position)
+			normals.append(face_normals[face])
+			colors.append(face_colors[face])
--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,14 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
+const SPEED = 30.0
 const JUMP_VELOCITY = 4.5
 const MOUSE_SENSITIVITY = 1.0
 
+var is_captured := true
+
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	get_viewport().debug_draw = Viewport.DEBUG_DRAW_WIREFRAME
 
 func _physics_process(delta: float) -> void:
 	## Add the gravity.
@@ -15,11 +18,19 @@ func _physics_process(delta: float) -> void:
 	## Handle jump.
 	#if Input.is_action_just_pressed("jump") and is_on_floor():
 		#velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_pressed('ui_cancel'):
+		if is_captured:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		
+		is_captured = not is_captured
 		
 	if Input.is_action_pressed('jump'):
-		position.y += 1 * delta
+		position.y += 5 * delta
 	if Input.is_action_pressed('ui_text_newline'):
-		position.y -= 1 * delta
+		position.y -= 5 * delta
 
 	# Get the input direction and handle the movement/deceleration.wwwwwwww
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -35,7 +46,7 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and is_captured:
 		rotate_y(deg_to_rad(-event.relative.x * MOUSE_SENSITIVITY))
 		$Head.rotate_x(deg_to_rad(-event.relative.y * MOUSE_SENSITIVITY))
 		$Head.rotation.x = clamp($Head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
