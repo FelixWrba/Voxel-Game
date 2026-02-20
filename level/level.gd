9f35d47ade5e17e89a6492f7780731ad24bdf070
Felix Wrba <felix.wrba@web.de>
2026-02-20 19:11:22 +0100

    Added multi-threaded chunk generation, Added radial chunk generation order, Extended PlayerController, Fixed bugs

--- level/level.gd
+++ level/level.gd
@@ -5,10 +5,17 @@ extends Node3D
 @export var cube_material: Material
 
 const CHUNK_SIZE = Vector3i(16, 128, 16)
-const RENDER_DISTANCE = 2
+const RENDER_DISTANCE = 4
+
+var previous_chunk_position: Vector3 = Vector3.ZERO
 
 var chunks: Dictionary = {}
+var chunk_queue: Array[Vector3] = []
 var noise = FastNoiseLite.new()
+
+var thread := Thread.new()
+var is_thread_running := false
+var is_chunk_pending := false
 
 enum Voxel { AIR, GRASS, DIRT, STONE, GRAVEL, SAND, SNOW }
 
@@ -20,11 +27,6 @@ var voxel_colors: Dictionary[Voxel, Color] = {
 	Voxel.SAND: Color.LIGHT_YELLOW,
 	Voxel.SNOW: Color.SNOW,
 }
-
-var c_surface_array: Array = []
-var c_vertices = PackedVector3Array()
-var c_normals = PackedVector3Array()
-var c_colors = PackedColorArray()
 
 var cube_vertices: Array[Vector3] = [
 	Vector3(-0.5, -0.5, 0.5),
@@ -71,16 +73,17 @@ func _ready() -> void:
 	noise.seed = 83
 	noise.fractal_octaves = 4
 	noise.frequency = 0.02
+	thread.start(_thread_loop)
 
 func _process(_delta: float) -> void:
 	var chunk_position := Vector3(
 		floor((player.position.x) / CHUNK_SIZE.x), 
-		floor(player.position.y / CHUNK_SIZE.y),
+		0,
 		floor((player.position.z) / CHUNK_SIZE.z)
 	)
 	remove_chunks(chunk_position)
-	spawn_chunks(chunk_position)
-	#create_chunk_mesh(create_chunk_data(Vector3.ZERO), Vector3.ZERO)
+	spawn_chunks(chunk_position, previous_chunk_position)
+	previous_chunk_position = chunk_position
 	
 func remove_chunks(chunk_pos: Vector3):
 	var start_x = chunk_pos.x - RENDER_DISTANCE
@@ -90,25 +93,46 @@ func remove_chunks(chunk_pos: Vector3):
 	
 	for chunk_x in chunks:
 		for chunk_z in chunks[chunk_x]:
+			# remove the chunk if outside of render distance
 			if chunk_x < start_x or chunk_x > end_x or chunk_z < start_z or chunk_z > end_z:
 				chunks[chunk_x][chunk_z].mesh.queue_free()
 				chunks[chunk_x].erase(chunk_z)
+			# remove empty chunk arrays from main array
 		if chunks[chunk_x].size() == 0:
 			chunks.erase(chunk_x)
 	
-func spawn_chunks(chunk_pos: Vector3) -> void:
-	for x in range(chunk_pos.x - RENDER_DISTANCE, chunk_pos.x + RENDER_DISTANCE + 1):
-		if not chunks.has(x):
-			chunks[x] = {}
-		for z in range(chunk_pos.z - RENDER_DISTANCE, chunk_pos.z + RENDER_DISTANCE + 1):
-			if not chunks[x].has(z):
-				var new_pos = Vector3(x, 0, z)
-				var new_chunk_data := create_chunk_data(new_pos)
-				var new_chunk_mesh := create_chunk_mesh(new_chunk_data, new_pos)
-				chunks[x][z] = {
-					'mesh': new_chunk_mesh,
-					'data': new_chunk_data
-				}
+func spawn_chunks(chunk_pos: Vector3, previous_pos: Vector3) -> void:
+	# empty the chunk queue when player position changes
+	if chunk_pos != previous_pos:
+		chunk_queue = []
+	# queue the chunks in ring order around the player
+	var current_ring := 0
+	while current_ring <= RENDER_DISTANCE:
+		for x in range(chunk_pos.x - current_ring, chunk_pos.x + current_ring + 1):
+			# create a chunk sub-array if it does not exist
+			if not chunks.has(x):
+				chunks[x] = {}
+			for z in range(chunk_pos.z - current_ring, chunk_pos.z + current_ring + 1):
+				# queue a chunk for creation when it: * does not exist * has not been queued 
+				var new_pos = Vector3(x, 0, z) 
+				if not chunks[x].has(z) and not chunk_queue.has(new_pos):
+					chunk_queue.append(new_pos)
+		
+		current_ring += 1
+
+func _thread_loop() -> void:
+	while true:
+		# try to create a chunk every 10 milliseconds when:
+		# * there are chunks to create
+		# * no chunk is being generated
+		if chunk_queue.size() > 0 and not is_chunk_pending:
+			is_chunk_pending = true
+			var chunk_pos = chunk_queue.pop_front()
+			var data = create_chunk_data(chunk_pos)
+			var arrays = create_chunk_arrays(data, chunk_pos)
+			# create the chunk on the main thread on idle
+			call_deferred("_create_chunk_mesh", chunk_pos, data, arrays)
+		OS.delay_msec(10)
 
 func create_chunk_data(chunk_pos: Vector3) -> Dictionary[Vector3, Voxel]:
 	var chunk: Dictionary[Vector3, Voxel] = {}
@@ -125,9 +149,8 @@ func create_chunk_data(chunk_pos: Vector3) -> Dictionary[Vector3, Voxel]:
 				var big = noise.get_noise_2d(global_x * 0.2, global_z * 0.2) * 64
 				var medium = noise.get_noise_2d(global_x * 0.5, global_z * 0.5) * 16
 				var small = noise.get_noise_2d(global_x, global_z) * 8
-				var GROUND_LEVEL = clamp(round(pow((big + medium + small + BASE_LEVEL), 1.1)), 4, 128)
+				var GROUND_LEVEL = clamp(round(pow((big + medium + small + BASE_LEVEL), 1.1)), 1.0, 128.0)
 				var voxel: Voxel = Voxel.AIR
-				
 				if y == GROUND_LEVEL:
 					if GROUND_LEVEL < (BASE_LEVEL - 2):
 						voxel = Voxel.GRAVEL
@@ -151,7 +174,7 @@ func create_chunk_data(chunk_pos: Vector3) -> Dictionary[Vector3, Voxel]:
 				chunk[Vector3(x, y, z)] = voxel
 	return chunk
 
-func create_chunk_mesh(chunk_data: Dictionary[Vector3, Voxel], chunk_pos: Vector3) -> MeshInstance3D:
+func create_chunk_arrays(chunk_data: Dictionary[Vector3, Voxel], chunk_pos: Vector3) -> Dictionary:
 	var vertices := PackedVector3Array()
 	var normals := PackedVector3Array()
 	var colors := PackedColorArray()
@@ -180,12 +203,24 @@ func create_chunk_mesh(chunk_data: Dictionary[Vector3, Voxel], chunk_pos: Vector
 		if is_air(chunk_data, pos + Vector3.FORWARD):
 			add_face_to_arrays(Face.BACK, world_pos, vertices, normals, colors, voxel_colors[voxel])
 	
+	return {
+		'vertices': vertices,
+		'normals': normals,
+		'colors': colors,
+	}
+
+func _create_chunk_mesh(chunk_pos: Vector3, data: Dictionary[Vector3, Voxel], chunk_arrays: Dictionary):
+	# cancel the chunk generation if it already exists
+	if chunks.has(chunk_pos.x) and chunks[chunk_pos.x].has(chunk_pos.z):
+		is_chunk_pending = false
+		return
+	
 	var surface_array := []
 	surface_array.resize(Mesh.ARRAY_MAX)
 	
-	surface_array[Mesh.ARRAY_VERTEX] = vertices
-	surface_array[Mesh.ARRAY_NORMAL] = normals
-	surface_array[Mesh.ARRAY_COLOR] = colors
+	surface_array[Mesh.ARRAY_VERTEX] = chunk_arrays.vertices
+	surface_array[Mesh.ARRAY_NORMAL] = chunk_arrays.normals
+	surface_array[Mesh.ARRAY_COLOR] = chunk_arrays.colors
 	
 	var array_mesh = ArrayMesh.new()
 	array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
@@ -200,7 +235,14 @@ func create_chunk_mesh(chunk_data: Dictionary[Vector3, Voxel], chunk_pos: Vector
 	static_body.add_child(collision_shape)
 	add_child(static_body)
 	
-	return mesh_instance
+	if not chunks.has(chunk_pos.x):
+		chunks[chunk_pos.x] = {}
+	
+	chunks[chunk_pos.x][chunk_pos.z] = {
+		'mesh': static_body,
+		'data': data,
+	}
+	is_chunk_pending = false
 
 func is_air(chunk_data: Dictionary[Vector3, Voxel], pos: Vector3) -> bool:
 	if not chunk_data.has(pos):
--- player/player.gd
+++ player/player.gd
@@ -1,23 +1,35 @@
 extends CharacterBody3D
 
+@onready var info_label: Label = $Head/Camera3D/InfoLabel
+
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const MOUSE_SENSITIVITY = 1.0
 
+const FLY_SPEED = 50.0
+const FLY_VELOCITY = 25.0
+
 var is_captured := true
+var fly_mode := true
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	#get_viewport().debug_draw = Viewport.DEBUG_DRAW_WIREFRAME
+	
+func _process(_delta: float) -> void:
+	info_label.text = 'FPS: ' + str(Engine.get_frames_per_second()) + '\nPositon: ' + str(position)
 
 func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed('ui_up'):
+		fly_mode = not fly_mode
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_pressed("jump") and (is_on_floor() or fly_mode):
+		velocity.y =  FLY_VELOCITY if fly_mode else JUMP_VELOCITY
 	
 	if Input.is_action_just_pressed('ui_cancel'):
 		if is_captured:
@@ -32,11 +44,11 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * (FLY_SPEED if fly_mode else SPEED)
+		velocity.z = direction.z * (FLY_SPEED if fly_mode else SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, FLY_SPEED if fly_mode else SPEED)
+		velocity.z = move_toward(velocity.z, 0, FLY_SPEED if fly_mode else SPEED)
 
 	move_and_slide()
 
